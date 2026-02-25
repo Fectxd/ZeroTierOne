0f0e6b3b163c4c7ab76e3643163a2b971b643c2e
Grant Limberg <grant.limberg@zerotier.com>
2026-02-25 13:37:04 -0800

    add a linked_id column to the oidc_config table.
    Required to get the list of configs  for a particular org that the controller has.  Named it `linked_id` rather than `org_id` since we don't know what it will be linked to in CV2

--- ext/central-controller-docker/migrations/0005_controller_log_and_sso.up.sql
+++ ext/central-controller-docker/migrations/0005_controller_log_and_sso.up.sql
@@ -21,9 +21,10 @@ CREATE INDEX IF NOT EXISTS sso_expiry_network_member_ix ON public.sso_expiry (ne
 
 CREATE TABLE IF NOT EXISTS oidc_config (
 	client_id TEXT NOT NULL,
+	linked_id TEXT NOT NULL,
 	issuer TEXT NOT NULL,
 	authorization_endpoint TEXT NOT NULL,
 	sso_impl_version BIGINT NOT NULL DEFAULT 1,
 	provider TEXT NOT NULL DEFAULT 'default',
-	PRIMARY KEY (client_id)
+	PRIMARY KEY (client_id, linked_id)
 );
