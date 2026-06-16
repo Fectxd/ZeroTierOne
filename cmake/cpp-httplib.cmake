ace53523883da592a0f007e1c77621b3f1d46bbf
Grant Limberg <grant.limberg@zerotier.com>
2026-06-15 17:41:09 -0700

    Bump cpp-httplib so it supports win32

--- cmake/cpp-httplib.cmake
+++ cmake/cpp-httplib.cmake
@@ -4,7 +4,10 @@ set(FETCHCONTENT_QUIET OFF)
 FetchContent_Declare(
     cpp-httplib
     GIT_REPOSITORY https://github.com/yhirose/cpp-httplib.git
-    GIT_TAG v0.25.0
+    # v0.47.0+: supports 32-bit Windows. v0.25.0 had a `#warning` refusing 32-bit
+    # Windows that MSVC's default preprocessor escalates to a fatal C1021 (breaks
+    # the windows-x86 build).
+    GIT_TAG v0.47.0
     GIT_SHALLOW ON
 )
 set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "")
